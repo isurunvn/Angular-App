bcd98eb057fe155b3ed48d1227f1e82200a3b098
inaveen00 <isurunaveen27@gmail.com>
2023-01-12 21:48:06 +0530

    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatTabsModule} from "@angular/material/tabs";
+import { NewCustomerComponent } from './Components/new-customer/new-customer.component';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatLegacyInputModule} from "@angular/material/legacy-input";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewCustomerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTabsModule,
+    MatFormFieldModule,
+    MatLegacyInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/new-customer/new-customer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-customer',
+  templateUrl: './new-customer.component.html',
+  styleUrls: ['./new-customer.component.scss']
+})
+export class NewCustomerComponent {
+
+}
